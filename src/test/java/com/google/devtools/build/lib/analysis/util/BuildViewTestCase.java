ed0657fe4cce4e767fd329d23a6454fb67a36c87
buchgr <buchgr@google.com>
2019-06-18 08:06:32 -0700

    Remove all usages of BlazeDirectories#(getExecRoot()|getOutputPath()) from Bazel
    Bazel's execution root path looks as follows: output_base/execroot/workspace_name. That is one needs to know the workspace name when constructing it. Bazel only knows the workspace name after/during the loading phase however BlazeDirectories is constructed before that. So BlazeDirectories.getExecRoot() returns a path to a folder that's not the exec root (it takes the name of the directory that contains the WORKSPACE file as the workspace name) and this leads to all kinds of bugs as one can imagine.
    
    Thus I removed all calls to BlazeDirectories.(getExecRoot()|getOutputPath()) in Bazel and replaced them with calls to BlazeDirectories.getExecRoot(workspaceName).
    
    What this change does:
     - All artifacts in Bazel agree on one exec root.
     - Bazel will no longer create two execroot directories on disk.
     - Renamed getExecRoot to getBlazeExecRoot() and updated the code to return null when called in Bazel in order to prevent future wrong use of this method.
     - Resolved a TODO in BlazeDirectories where we had code to support the workspace path being null.
    
    RELNOTES: None.
    PiperOrigin-RevId: 253790673

--- src/test/java/com/google/devtools/build/lib/analysis/util/BuildViewTestCase.java
+++ src/test/java/com/google/devtools/build/lib/analysis/util/BuildViewTestCase.java
@@ -234,6 +234,7 @@ public abstract class BuildViewTestCase extends FoundationTestCase {
             rootDirectory,
             /* defaultSystemJavabase= */ null,
             analysisMock.getProductName());
+
     actionKeyContext = new ActionKeyContext();
     mockToolsConfig = new MockToolsConfig(rootDirectory, false);
     mockToolsConfig.create("/bazel_tools_workspace/WORKSPACE", "workspace(name = 'bazel_tools')");
@@ -257,7 +258,7 @@ public abstract class BuildViewTestCase extends FoundationTestCase {
     workspaceStatusActionFactory = new AnalysisTestUtil.DummyWorkspaceStatusActionFactory();
     mutableActionGraph = new MapBasedActionGraph(actionKeyContext);
     ruleClassProvider = getRuleClassProvider();
-
+    getOutputPath().createDirectoryAndParents();
     ImmutableList<PrecomputedValue.Injected> extraPrecomputedValues =
         ImmutableList.of(
             PrecomputedValue.injected(PrecomputedValue.REPO_ENV, ImmutableMap.<String, String>of()),
@@ -1503,7 +1504,7 @@ public abstract class BuildViewTestCase extends FoundationTestCase {
   }
 
   protected Path getOutputPath() {
-    return directories.getOutputPath();
+    return directories.getOutputPath(ruleClassProvider.getRunfilesPrefix());
   }
 
   /**
