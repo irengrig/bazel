be25d2f9feec9a5cce1338dfe52e0e9bb1d87c70
ichern <ichern@google.com>
2019-09-18 14:29:10 -0400

    Changes for using Bazel include scanning for ninja targets
    Problem: no grep-includes.sh for Bazel

--- src/main/java/com/google/devtools/build/lib/bazel/rules/ninja/NinjaGenRuleAction.java
+++ src/main/java/com/google/devtools/build/lib/bazel/rules/ninja/NinjaGenRuleAction.java
@@ -1,40 +1,49 @@
 package com.google.devtools.build.lib.bazel.rules.ninja;
 
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.devtools.build.lib.actions.ActionEnvironment;
 import com.google.devtools.build.lib.actions.ActionExecutionContext;
 import com.google.devtools.build.lib.actions.ActionExecutionException;
 import com.google.devtools.build.lib.actions.ActionOwner;
 import com.google.devtools.build.lib.actions.Artifact;
 import com.google.devtools.build.lib.actions.CommandLines;
+import com.google.devtools.build.lib.actions.ExecException;
 import com.google.devtools.build.lib.actions.RunfilesSupplier;
+import com.google.devtools.build.lib.includescanning.IncludeScannerSupplierImpl;
 import com.google.devtools.build.lib.rules.cpp.CppFileTypes;
+import com.google.devtools.build.lib.rules.cpp.CppIncludeScanningUtil;
+import com.google.devtools.build.lib.rules.cpp.IncludeScanner.IncludeScannerSupplier;
+import com.google.devtools.build.lib.rules.cpp.IncludeScanner.IncludeScanningHeaderData;
+import com.google.devtools.build.lib.rules.cpp.IncludeScanning;
 import com.google.devtools.build.lib.rules.genrule.GenRuleAction;
 import com.google.devtools.build.lib.util.DependencySet;
 import com.google.devtools.build.lib.util.io.process.PathUtils;
-import com.google.devtools.build.lib.util.io.process.ProcessParameters;
-import com.google.devtools.build.lib.util.io.process.ProcessResult;
-import com.google.devtools.build.lib.util.io.process.ProcessRunner;
 import com.google.devtools.build.lib.vfs.Path;
+import com.google.devtools.build.lib.vfs.PathFragment;
 import com.google.devtools.build.lib.vfs.Root;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
 
 public class NinjaGenRuleAction extends GenRuleAction {
 
   private final Path workspaceRoot;
   private final Path depfilePath;
   private final String rawCommandLine;
+  private final IncludeScannerSupplier includeScannerSupplier;
+  private final Artifact grepIncludes;
   private Set<Path> discoveredInputs;
 
   public NinjaGenRuleAction(ActionOwner owner,
@@ -48,12 +57,17 @@ public class NinjaGenRuleAction extends GenRuleAction {
       CharSequence progressMessage,
       Path workspaceRoot,
       Path depfilePath,
-      String rawCommandLine) {
+      String rawCommandLine,
+      IncludeScannerSupplier includeScannerSupplier,
+      Artifact grepIncludes) {
     super(owner, tools, inputs, outputs, commandLines, env, executionInfo, runfilesSupplier,
         progressMessage);
     this.workspaceRoot = workspaceRoot;
     this.depfilePath = depfilePath;
     this.rawCommandLine = rawCommandLine;
+    // todo use that
+    this.includeScannerSupplier = includeScannerSupplier;
+    this.grepIncludes = grepIncludes;
   }
 
   @Override
@@ -66,30 +80,48 @@ public class NinjaGenRuleAction extends GenRuleAction {
       throws ActionExecutionException, InterruptedException {
     // todo see CppCompileAction#discoverInputs
 
-    // todo - have a shared executor :(
-    ExecutorService executorService = Executors.newFixedThreadPool(2);
-    // todo should not be the exec root because of caching
-    try {
-      String jsonPath = generateJsonInput(actionExecutionContext);
-      ProcessParameters processParameters = ProcessParameters.builder()
-          // currently it is hardcoded, but should not be
-          .setName("clang-scan-deps")
-          .setWorkingDirectory(actionExecutionContext.getExecRoot().getPathFile())
-          .setArguments("--compilation-database", jsonPath)
-          .build();
-      // commented out as it does not work now
-      // ProcessResult processResult = new ProcessRunner(processParameters, executorService)
-      //     .runSynchronously();
-      // System.out.println(processResult.outString());
+    Iterable<Artifact> discoveredInputArtifacts = discoverCppHeadersBazel(actionExecutionContext);
+    discoveredInputs = Sets.newHashSet();
+    for (Artifact artifact : discoveredInputArtifacts) {
+      discoveredInputs.add(artifact.getPath());
+    }
+    return discoveredInputArtifacts;
+  }
 
-      List<Artifact> discoveredInputArtifacts = Collections.emptyList();
-      discoveredInputs = Sets.newHashSet();
-      for (Artifact artifact : discoveredInputArtifacts) {
-        discoveredInputs.add(artifact.getPath());
+  private Iterable<Artifact> discoverCppHeadersBazel(ActionExecutionContext actionExecutionContext)
+      throws ActionExecutionException, InterruptedException {
+    String[] parts = rawCommandLine.split(" ");
+    List<String> options = Lists.newArrayList(Arrays.asList(parts).subList(1, parts.length));
+    List<PathFragment> systemIncludeDirs = CppIncludeScanningUtil.getSystemIncludeDirs(
+        options, this::prettyPrint);
+    List<String> cmdlineIncludes = CppIncludeScanningUtil.getCmdlineIncludes(options);
+
+    ImmutableMap.Builder<PathFragment, Artifact> pathToLegalOutputArtifact =
+        ImmutableMap.builder();
+    ImmutableList<Artifact> inputsCopy;
+    synchronized (this) {
+      for (Artifact path : outputs) {
+        pathToLegalOutputArtifact.put(path.getExecPath(), path);
       }
-      updateInputs(discoveredInputArtifacts);
-      return discoveredInputArtifacts;
-    } catch (Exception e) {
+      inputsCopy = ImmutableList.copyOf(inputs);
+    }
+
+    try {
+      Set<Artifact> includes = Sets.newConcurrentHashSet();
+      IncludeScanningHeaderData includeScanningHeaderData = new IncludeScanningHeaderData.Builder(
+          pathToLegalOutputArtifact.build(), ImmutableSet.of())
+          .setSystemIncludeDirs(systemIncludeDirs)
+          .setCmdlineIncludes(cmdlineIncludes)
+          .build();
+      ListenableFuture<?> listenableFuture = includeScannerSupplier
+          .scannerFor(Collections.emptyList(), ImmutableList.copyOf(systemIncludeDirs))
+          .processAsync(null, inputsCopy, includeScanningHeaderData,
+              cmdlineIncludes, includes, this, actionExecutionContext, grepIncludes);
+      ListenableFuture<Iterable<Artifact>> future = IncludeScanning
+          .collectIncludes(listenableFuture, actionExecutionContext,
+              includes, ImmutableList.of());
+      return future.get();
+    } catch (ExecutionException | IOException | ExecException e) {
       throw new ActionExecutionException(e, this, true);
     }
   }
